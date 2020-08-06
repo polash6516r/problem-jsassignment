699b6f3db57ba4a5ee78439c04f9e308bc47537b
polash6516r <polash6516r@gmail.com>
2020-08-06 23:11:38 +0600

    problem solved

---
+++ problmAssignment.js
@@ -0,0 +1,36 @@
+ //feetToMile
+ function feetToMile(feet){
+     var Mile = feet / 5280;
+     return Mile;
+ }
+ var result = feetToMile(15840);
+ console.log(result);
+
+
+
+ // WoodCalculator
+ function WoodCalculator(chair, table, bed){
+     var chairCount = chair * 1;
+     var tableCount = table * 3;
+     var bedCount = bed * 5;
+     var totalWood = chairCount + tableCount + bedCount;
+     return totalWood;
+ }
+
+
+ // brickCalculator
+
+
+ //tinyFriend
+  function tinyFriend(friends){
+     var smallest = friends[0];
+     for (i = 0; i < friends.length; i++){
+         var length = friends[i];
+         if(length> smallest){
+             smallest = length;
+         }
+     }
+     return smallest;
+}
+
+  
